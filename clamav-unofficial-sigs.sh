522e4b37b41bb0be778e1cf677a94124bb73a5c8
eXtremeSHOK.com <admin@extremeshok.com>
2016-03-31 20:01:06 +0200

    add fallback to host if dig returns no records

--- clamav-unofficial-sigs.sh
+++ clamav-unofficial-sigs.sh
@@ -306,7 +306,7 @@ if [ "$we_have_a_config" == "0" ] ; then
 	exit 1
 fi
 
-#prevent some issues with only a user.conf being loaded
+#prevent some issues with an incomplete or only a user.conf being loaded
 if [ $config_version  == "0" ] ; then
 	xshok_pretty_echo_and_log "ERROR: Config file is missing important contents of the master.conf" "="
 	xshok_pretty_echo_and_log "Note: Possible fix would be to point the script to the dir with the configs"
@@ -1139,9 +1139,20 @@ if [ "$sanesecurity_enabled" == "yes" ] ; then
 		
 		db_file=""
 		xshok_pretty_echo_and_log "Sanesecurity Database & GPG Signature File Updates" "="
+
 		sanesecurity_mirror_ips=`dig +ignore +short $sanesecurity_url`
+		#add fallback to host if dig returns no records
+		if [ `xshok_array_count  "$sanesecurity_mirror_ips"` -lt 1 ] ; then
+			sanesecurity_mirror_ips=`host -t A "$sanesecurity_url" | sed -n '/has address/{s/.*address \([^ ]*\).*/\1/;p}'`
+		fi
+
 		for sanesecurity_mirror_ip in $sanesecurity_mirror_ips ; do
+			sanesecurity_mirror_name=""
 			sanesecurity_mirror_name=`dig +short -x $sanesecurity_mirror_ip | command sed 's/\.$//'`
+			#add fallback to host if dig returns no records
+			if [ "$sanesecurity_mirror_name" == "" ] ; then
+				sanesecurity_mirror_name=`host "$sanesecurity_mirror_ip" | sed -n '/name pointer/{s/.*pointer \([^ ]*\).*/\1/;p}'`
+			fi
 			sanesecurity_mirror_site_info="$sanesecurity_mirror_name $sanesecurity_mirror_ip"
 			xshok_pretty_echo_and_log "Sanesecurity mirror site used: $sanesecurity_mirror_site_info"
 			rsync $rsync_output_level $no_motd --files-from=$sanesecurity_include_dbs -ctuz $connect_timeout --timeout="$rsync_max_time" --stats rsync://$sanesecurity_mirror_ip/sanesecurity $sanesecurity_dir 2>/dev/null
