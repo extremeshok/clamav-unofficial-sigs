3edaa601ecfa867979d2b2f343bb17c92d2ce997
eXtremeSHOK.com <admin@extremeshok.com>
2016-03-31 04:39:28 +0200

    travis-ci

--- .travis/ci-test.sh
+++ .travis/ci-test.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/musl/bin:$HOME/bin
 
 pwd
 
-bash clamav-unofficial-sigs.sh
+if bash clamav-unofficial-sigs.sh ; then
+	echo .. OK
+else
+ 	echo .. ERROR
+  exit 1
+fi
