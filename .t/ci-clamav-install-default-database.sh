a4ef246ce761d07e768639d704a1674856501e3b
eXtremeSHOK <admin@extremeshok.com>
2017-03-15 04:19:49 +0200

    Travis fixes

--- .t/ci-clamav-install-default-database.sh
+++ .t/ci-clamav-install-default-database.sh
@@ -9,7 +9,7 @@ echo "Installing latest clamav databases"
 mkdir -p /var/lib/clamav
 cp -f bytecode.cvd /var/lib/clamav/bytecode.cvd
 cp -f daily.cvd /var/lib/clamav/daily.cvd
-cp -f bytecode.cvd /var/lib/clamav/main.cvd
+cp -f main.cvd /var/lib/clamav/main.cvd
 chown -R clamav:clamav /var/lib/clamav
 service clamav-daemon start
 if [ "$?" -eq "0" ] ; then
