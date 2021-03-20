a9afb231dc3de77b0e25e8134507dcf5297e07a4
eXtremeSHOK <admin@extremeshok.com>
2021-03-20 04:35:04 +0200

    7.2.5

--- clamav-unofficial-sigs.sh
+++ clamav-unofficial-sigs.sh
@@ -1550,8 +1550,8 @@ EOF
 ################################################################################
 
 # Script Info
-script_version="7.2.4"
-script_version_date="2021-03-17"
+script_version="7.2.5"
+script_version_date="2021-03-20"
 minimum_required_config_version="96"
 minimum_yara_clamav_version="0.100"
 
